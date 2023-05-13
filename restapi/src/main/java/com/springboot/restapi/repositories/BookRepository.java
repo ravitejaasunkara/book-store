a478b1171ec4aaa55b64f07b0995577f5804d431
Raviteja Sunkara <101201768+ravitejaasunkara@users.noreply.github.com>
2023-05-13 21:51:35 +0530

    Update BookRepository.java

--- restapi/src/main/java/com/springboot/restapi/repositories/BookRepository.java
+++ restapi/src/main/java/com/springboot/restapi/repositories/BookRepository.java
@@ -1,9 +1,10 @@
 package com.springboot.restapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springboot.restapi.entities.Book;
-
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer>{
 
 }
